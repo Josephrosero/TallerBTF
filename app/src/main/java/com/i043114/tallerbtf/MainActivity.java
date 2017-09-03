192ffce6f856e652dda6260c7b7c0811047df34c
Josephrosero <josephrosero47@gmail.com>
2017-09-02 23:25:29 -0500

    Initial commit

---
+++ app/src/main/java/com/i043114/tallerbtf/MainActivity.java
@@ -0,0 +1,37 @@
+package com.i043114.tallerbtf;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.flt_1);
+        registerForContextMenu(fab);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        menu.setHeaderTitle("Mis Opciones");
+        inflater.inflate(R.menu.menu2, menu);
+    }
+}
